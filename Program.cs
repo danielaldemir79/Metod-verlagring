483ed02a7f52ca2bd5cb59307f0ae50d65c1a0db
Daniel Aldemir <daniel.aldemir79@gmail.com>
2025-09-15 20:39:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Metodöverlagring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(helaNamnet("Daniel", "Aldemir"));
+
+            Console.WriteLine(helaNamnet("Daniel", "Dani", "Aldemir"));
+
+
+            Console.WriteLine(helaNamnet("daniel", "aldemir", true));
+
+        }
+    
+
+    static string helaNamnet(string förnamn, string efternamn)
+        {
+            var fullständigtNamn = $"{förnamn} {efternamn}";
+            return fullständigtNamn;
+
+        }
+
+        static string helaNamnet(string förnamn, string mellannamn, string efternamn)
+        {
+            var fullständigtNamn = $"{förnamn} { mellannamn}  { efternamn}";
+            return fullständigtNamn;
+
+        }
+
+
+        static string helaNamnet(string förnamn, string efternamn, bool check)
+        {
+            var full = $"{förnamn} {efternamn}";
+            return check ? full.ToUpper() : full;
+        }
+
+
+    }
+}
